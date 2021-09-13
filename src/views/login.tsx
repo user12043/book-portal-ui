4e7293505c8561a9c7a51e51dd5a3e5520feff3e
user12043 <user12043@hotmail.com>
2021-09-13 21:55:39 +0300

    registration done

--- src/views/login.tsx
+++ src/views/login.tsx
@@ -6,19 +6,22 @@ import { apiReq } from "utils";
 const Login: FC = () => {
   const { appDispatch } = useContext(AppContext);
   const [loginUser, setLoginUser] = useState<User | null>(null);
+  const [register, setRegister] = useState(false);
 
   const login = () => {
     loginUser &&
-      apiReq("login", {
-        method: "POST",
-        body: loginUser
+      apiReq(!register ? "login" : "users", {
+        method: !register ? "POST" : "PUT",
+        body: { ...loginUser, role: "USER" }
       }).then(user =>
-        user
-          ? appDispatch({
-              type: "LOGIN",
-              payload: user
-            })
-          : alert("login failed")
+        !register
+          ? user
+            ? appDispatch({
+                type: "LOGIN",
+                payload: user
+              })
+            : alert("login failed")
+          : setRegister(false)
       );
   };
 
@@ -30,15 +33,45 @@ const Login: FC = () => {
   return (
     <div className="row h-100 align-items-center px-2">
       <form
-        className="col-md-4 offset-md-4 bg-dark p-3 rounded"
+        className="form col-md-4 offset-md-4 bg-dark p-3 rounded"
         onSubmit={e => {
           e.preventDefault();
           login();
         }}
       >
-        <h1 className="mb-3">Book Portal Login</h1>
+        <h1 className="mb-3">Book Portal {!register ? "Login" : "Register"}</h1>
+        {register && (
+          <>
+            <div className="mb-3 input-group">
+              <i className="input-group-text bi bi-tag" id="usernameAddon"></i>
+              <input
+                type="text"
+                className="form-control"
+                name="username"
+                aria-describedby="usernameAddon"
+                placeholder="username"
+                value={loginUser?.username || ""}
+                onChange={change}
+                required
+              />
+            </div>
+            <div className="mb-3 input-group">
+              <i className="input-group-text bi bi-dash" id="nameAddon"></i>
+              <input
+                type="text"
+                className="form-control"
+                name="name"
+                aria-describedby="nameAddon"
+                placeholder="name"
+                value={loginUser?.name || ""}
+                onChange={change}
+                required
+              />
+            </div>
+          </>
+        )}
         <div className="input-group">
-          <i className="input-group-text bi bi-person" id="usernameAddon"></i>
+          <i className="input-group-text bi bi-person" id="emailAddon"></i>
           <input
             type="email"
             className="form-control"
@@ -66,14 +99,16 @@ const Login: FC = () => {
             required
           />
         </div>
-        <div className="mb-3 form-check fs-6">
-          <input type="checkbox" className="form-check-input" id="rememberMe" />
-          <label className="form-check-label" htmlFor="rememberMe">
-            Remember me
-          </label>
+        <div className="ms-auto w-50 mb-3 fs-6">
+          <i>
+            {!register && "No account?  "}
+            <a href="#/#" onClick={() => setRegister(!register)}>
+              {!register ? "Register" : "Login"}
+            </a>
+          </i>
         </div>
         <button type="submit" className="btn btn-primary">
-          Login
+          {!register ? "Login" : "Register"}
         </button>
       </form>
     </div>
